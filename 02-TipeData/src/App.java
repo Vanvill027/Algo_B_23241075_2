7fba0f934ff887170989811e4e85c1b2c5cca6c4
Vanvill027 <146050531+Vanvill027@users.noreply.github.com>
2023-11-28 12:58:23 +0800

    Kuliah Algo 28 November 2023

--- 02-TipeData/src/App.java
+++ 02-TipeData/src/App.java
@@ -14,7 +14,7 @@ public class App {
         System.out.println("Nilai MIN : " + Integer.MIN_VALUE);
         System.out.println("Besar Integer : " + Integer.SIZE + "bit");
 
-        // byte
+        // byte (satuan)
         byte b = 10;
         System.out.println("======= BYTE =======");
         System.out.println("Nilai Byte b = " + b);
@@ -22,7 +22,7 @@ public class App {
         System.out.println("Nilai MIN : " + Byte.MIN_VALUE);
         System.out.println("Besar Byte : " + Byte.SIZE + "bit");
 
-        // short
+        // short (satuan)
         short s = 10;
         System.out.println("======= SHORT =======");
         System.out.println("Nilai Short s = " + s);
@@ -30,7 +30,7 @@ public class App {
         System.out.println("Nilai MIN : " + Short.MIN_VALUE);
         System.out.println("Besar Short : " + Short.SIZE + "bit");
 
-        // long
+        // long (satuan)
         long l = 10;
         System.out.println("======= LONG =======");
         System.out.println("Nilai Long l = " + l);
@@ -38,7 +38,7 @@ public class App {
         System.out.println("Nilai MIN : " + Long.MIN_VALUE);
         System.out.println("Besar Long : " + Long.SIZE + "bit");
 
-        // double
+        // double (koma, bilangan real)
         double d = 10;
         System.out.println("======= DOUBLE =======");
         System.out.println("Nilai Double d = " + d);
@@ -46,7 +46,7 @@ public class App {
         System.out.println("Nilai MIN : " + Double.MIN_VALUE);
         System.out.println("Besar Double : " + Double.SIZE + "bit");
 
-        // float
+        // float (koma, bilangan real)
         float f = 10;
         System.out.println("======= FLOAT =======");
         System.out.println("Nilai Float = " + f);
@@ -54,17 +54,18 @@ public class App {
         System.out.println("Nilai MIN : " + Float.MIN_VALUE);
         System.out.println("Besar Float : " + Float.SIZE + "bit");
 
-        // char
+        // char (karakter) berdasarkan ASCII
         char c = 100;
         System.out.println("======= CHAR =======");
         System.out.println("Nilai Char c = " + c);
         System.out.println("Besar Char : " + Character.SIZE + "bit");
 
-        // boolean
-        int bl = 10;
+        // boolean (nilai true atau false)
+        boolean val = false;
         System.out.println("======= BOOLEAN =======");
-        System.out.println("Nilai Boolean bl = " + bl);
-        System.out.println("Besar Boolean : " + Byte.SIZE + "bit");
+        System.out.println("nilai boolean val = " + val);
+        System.out.println("Nilai max = " + Boolean.TRUE);
+        System.out.println("Nilai min = " + Boolean.FALSE);
 
     }
 }
